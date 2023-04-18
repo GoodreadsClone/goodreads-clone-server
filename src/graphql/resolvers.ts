c8404698dca0ee78ee2f9435bdadfcc00710246f
Rohan Samavedam <rohansamavedam@gmail.com>
2023-04-17 19:44:05 -0700

    Add basic graphql support with apollo server

---
+++ src/graphql/resolvers.ts
@@ -0,0 +1,18 @@
+const books = [
+    {
+      id: '1',
+      title: 'The Awakening',
+      author: 'Kate Chopin',
+    },
+    {
+      id: '2',
+      title: 'City of Glass',
+      author: 'Paul Auster',
+    },
+];
+
+export const resolvers = {
+    Query: {
+      books: () => books,
+    },
+};
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,17 @@
-console.log("this is index")
+import { ApolloServer } from '@apollo/server';
+import { startStandaloneServer } from '@apollo/server/standalone';
+import { readFileSync } from 'fs';
+import { resolvers } from './graphql/resolvers.js';
+
+const typeDefs = readFileSync('./src/graphql/schema.graphql', { encoding: 'utf-8' });
+
+const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+}); 
+
+const { url } = await startStandaloneServer(server, {
+listen: { port: 4000 },
+});
+
+console.log(`🚀  Server ready at: ${url}`);

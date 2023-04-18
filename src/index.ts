f18f13d98ea22643431168704dcf3ab87602b32d
Rohan Samavedam <rohansamavedam@gmail.com>
2023-04-17 23:55:58 -0700

    Link postgresSQL DB using TypeORM

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,13 @@ import { ApolloServer } from '@apollo/server';
 import { startStandaloneServer } from '@apollo/server/standalone';
 import { readFileSync } from 'fs';
 import { resolvers } from './graphql/resolvers.js';
+import { PostgresDataSource } from './databaseconfig.js';
+
+PostgresDataSource.initialize().then(() => {
+    console.log("Data Source has been initialized!")
+}).catch((err) => {
+    console.error("Error during Data Source initialization", err)
+})
 
 const typeDefs = readFileSync('./src/graphql/schema.graphql', { encoding: 'utf-8' });
 
